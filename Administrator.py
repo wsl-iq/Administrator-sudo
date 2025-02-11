495454cb0a7b2ac207f049cb957199693109b3f9
Mohammed Al-Baqer <mohmmadkasper@gmail.com>
2025-02-11 18:50:09 +0300

    Update Administrator.py

--- Administrator.py
+++ Administrator.py
@@ -1,46 +1,27 @@
-import os; os.system('cls' if os.name == 'nt' else 'clear')
-import sys
-import ctypes
+import ctypes, sys, os; os.system('cls' if os.name == 'nt' else 'clear')
 
-R = "\033[91;1m"  # Red
-G = "\033[92;1m"  # Green
-B = "\033[94;1m"  # Blue
-Y = "\033[93;1m"  # Yellow
-C = "\033[96;1m"  # Cyan
-M = "\033[95;1m"  # Magenta
-W = "\033[97;1m"  # White
-D = "\033[90;1m"  # Grey
-S = "\033[0m"     # Reset
-
-ERROR = "\033[93;1m" + "[" + "\033[91;1m" + "ERROR" + "\033[93;1m" + "]" + "\033[91;1m"
-sign = "\033[92;1m" + "[" + "\033[94;1m" + "*" + "\033[92;1m" + "]" + "\033[94;1m"
-INFO = "\033[93;1m" + "[" + "\033[92;1m" + "INFO" + "\033[93;1m" + "]" + "\033[94;1m"
-
-def administrator():
-    f"""{W}
-    (Run this program as an administrator)
-    """
+# Requires Administrator
+def Administrator():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
+    
+if not Administrator():
+    print("Program requires Administrator privileges")
+    script = sys.executable
+    params = " ".join([f'"{arg}"' for arg in sys.argv])
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", script, params, None, 1)
+    sys.exit()
 
+# python
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+# Run Program main
 
-def RunAsAdministrator(executable_path):
-    try:
-        ctypes.windll.shell32.ShellExecuteW(
-            None, "runas", executable_path, None, None, 1
-        )
-    except Exception as e:
-        print(f"{ERROR} Operation Error {M}{executable_path} {R}with Administrator!{W}")
-        sys.exit(1)
+print("Welcome I'm Mohammed Al-Baqer")
+try:
+    pass
 
-if __name__ == "__main__":
-    # Location program for run by Administrator
-    file_path = "app\\ManagerPc.py"
-
-    if not administrator():
-        print(f"{sign} Program required {Y}Manager pc {B}with Administrator...{W}")
-        RunAsAdministrator(file_path)
-        sys.exit(0)
-    print(f"{INFO} Operation Successful {Y}Manager pc {B}with Administrator.")
+except Exception as e:
+    print(f"Failed to execute the program: {e}")
+    input("Press Enter to exit...")
